6bb1b6ba4dd73e981c63a3ca7bc18e092b23f0dd
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-07 22:38:52 +0100

    Create an Orthographic projection class

---
+++ core/maths/Orthographic.cpp
@@ -0,0 +1,94 @@
+/*
+// Orthographic Projection
+*/
+#include "Orthographic.hpp"
+
+/** Initialize Variables **/
+Orthographic::Orthographic(const Vec2& _vSize, float _fScale)
+    : m_View(IdentityMatrix), m_Projection(IdentityMatrix),
+    m_Position(Vec3(0.0f, 0.0f, 1.0f)), m_Origin(Vec3(0.0f))
+{
+    m_Scale = _fScale;
+    Resize(_vSize);
+
+    __ResetOrthographic();
+}
+
+/** Empty **/
+Orthographic::~Orthographic()
+{
+}
+
+/*! __ResetOrthographic Implementation
+**/
+void Orthographic::__ResetOrthographic(void)
+{
+	float Left      = -m_Scale * m_AspectRatio * 0.5f;
+	float Right     =  m_Scale * m_AspectRatio * 0.5f;
+	float Bottom    = -m_Scale * 0.5f;
+	float Top       =  m_Scale * 0.5f;
+
+    float zNear     = 0.1f;
+    float zFar      = 100.0f;
+
+    m_Projection = glm::ortho(Left, Right, Bottom, Top, zNear, zFar);
+    m_View = glm::lookAt(m_Position, m_Origin, Vec3(0.0f, 1.0f, 0.0f));
+}
+
+
+/*! Update Implementation
+**/
+void Orthographic::Update(void)
+{
+    __ResetOrthographic();
+}
+
+/*! Translate Implementation
+**/
+void Orthographic::Translate(const Vec3& _vTranslation)
+{
+    m_Position += _vTranslation;
+}
+
+/*! TranslateOrigin Implementation
+**/
+void Orthographic::TranslateOrigin(const Vec3& _vTranslation)
+{
+    m_Origin += _vTranslation;
+}
+
+/*! Resize Implementation
+**/
+void Orthographic::Resize(const Vec2& _vSize)
+{
+    m_AspectRatio = _vSize.x / _vSize.y;
+    m_Size = _vSize;
+}
+
+/*! SetScale Implementation
+**/
+void Orthographic::SetScale(float _fScale)
+{
+    m_Scale = _fScale;
+}
+
+/*! SetPosition Implementation
+**/
+void Orthographic::SetPosition(const Vec3& _vPosition)
+{
+    m_Position = _vPosition;
+}
+
+/*! SetOrigin Implementation
+**/
+void Orthographic::SetOrigin(const Vec3& _vPosition)
+{
+    m_Origin = _vPosition;
+}
+
+/*! GetViewProjection Implementation
+**/
+Mat4 Orthographic::GetViewProjection(void)
+{
+    return m_Projection * m_View;
+}
---
+++ core/maths/Orthographic.hpp
@@ -0,0 +1,79 @@
+/*
+// Orthographic Projection
+*/
+
+/** core/maths/Orthographic.hpp Gaurd Block **/
+#ifndef _GX_MATHS_ORTHOGRAPHIC_HEADER_
+#define _GX_MATHS_ORTHOGRAPHIC_HEADER_
+
+/** Include **/
+#include "Math.hpp"
+
+/**
+ * Orthographic Projection
+ * USAGE: Camera
+ **/
+class Orthographic
+{
+private:
+    Mat4 m_View;
+    Mat4 m_Projection;
+
+    Vec2 m_Size;
+    Vec3 m_Position;
+    Vec3 m_Origin;
+
+    float m_Scale;
+    float m_AspectRatio;
+
+    void __ResetOrthographic(void);
+
+public:
+    Orthographic(const Vec2& _vSize, float _fScale);
+    ~Orthographic();
+
+    /*! @brief Update projection math
+    **/
+    void Update(void);
+
+    /*! @brief Translate the view by _vTranslation
+    **/
+    void Translate(const Vec3& _vTranslation);
+
+    /*! @brief Translate the origin by _vTranslation
+    **/
+    void TranslateOrigin(const Vec3& _vTranslation);
+
+    /*! @brief Resize the view to _vSize
+    **/
+    void Resize(const Vec2& _vSize);
+
+    /*! @brief Set scale of the view to _fScale
+    **/
+    void SetScale(float _fScale);
+
+    /*! @brief Set the position of the view to _vPosition
+    **/
+    void SetPosition(const Vec3& _vPosition);
+
+    /*! @brief Set the origin of the view to _vPosition
+    **/
+    void SetOrigin(const Vec3& _vPosition);
+
+    /*! @brief Return the ViewProjection matrix
+    **/
+	Mat4 GetViewProjection(void);
+
+    /*! @brief Call "GetViewProjection()"
+    **/
+	inline operator Mat4(void)
+	{ return GetViewProjection(); }
+
+    /*! @brief Call "GetViewProjection()"
+    **/
+	inline operator glm::mat4(void)
+	{ return GetViewProjection(); }
+
+};
+
+#endif
