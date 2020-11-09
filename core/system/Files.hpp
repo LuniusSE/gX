e546d7b43646923d91b3110393c1fcdc7a5fa726
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-09 23:46:49 +0100

    Create Files.hpp

---
+++ core/system/Files.hpp
@@ -0,0 +1,48 @@
+/*
+// Files
+*/
+
+#ifndef _GX_SYSTEM_FILES_HEADER_
+#define _GX_SYSTEM_FILES_HEADER_
+
+/** Headers **/
+#include <Common.hpp>
+
+_GX_BEGIN
+
+/**
+ * Useful file information
+ *  File = ReadFile(_sPath)
+ **/
+struct File
+{
+    const char* sPath;
+    char* sData;
+    int nLines;
+    int nCharacters;
+};
+
+/** File Options **/
+enum class FileOptions
+{
+    Read,
+    Overwrite,
+    Append,
+    Delete,
+};
+
+/*! @brief Wrapper of std::filesystem::path
+ * 
+ * @param[in] _sPath Relative Path
+ * @returns Absolute Path
+ **/
+const char* FindPath(const char* _sPath);
+
+/*! @brief Open file
+ * TODO: Impl
+**/
+Status OpenFile(File& _fFile, FileOptions _eOptions);
+
+_GX_END
+
+#endif
