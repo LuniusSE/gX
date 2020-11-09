bd4fbe93bf44a0aa51a607f09ef7feade114fa26
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-09 23:46:00 +0100

    Include common and use namespace macros

--- core/maths/Math.hpp
+++ core/maths/Math.hpp
@@ -10,22 +10,29 @@
 #define DefaultPrecision glm::lowp
 
 /** Headers **/
+#include <Common.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtx/transform.hpp>
+
+_GX_BEGIN
 
 /******* Types *******/
 
 /**     Vectors     **/
-using Vec2 = glm::vec<2, float, DefaultPrecision>;
-using Vec3 = glm::vec<3, float, DefaultPrecision>;
-using Vec4 = glm::vec<4, float, DefaultPrecision>;
+
+using Vec2 = glm::vec<2, Float, DefaultPrecision>;
+using Vec3 = glm::vec<3, Float, DefaultPrecision>;
+using Vec4 = glm::vec<4, Float, DefaultPrecision>;
 
 /**     Matrices    **/
-using Mat2 = glm::mat<2, 2, float, DefaultPrecision>;
-using Mat3 = glm::mat<3, 3, float, DefaultPrecision>;
-using Mat4 = glm::mat<4, 4, float, DefaultPrecision>;
+
+using Mat2 = glm::mat<2, 2, Float, DefaultPrecision>;
+using Mat3 = glm::mat<3, 3, Float, DefaultPrecision>;
+using Mat4 = glm::mat<4, 4, Float, DefaultPrecision>;
 #define IdentityMatrix Mat4(1.0f)
 
 /*********************/
+
+_GX_END
 
 #endif
