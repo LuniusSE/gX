673aaaa21f1eebe516c0b24962ae0d2965b483a1
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-09 23:44:56 +0100

    Create Namespace.hpp

---
+++ core/common/Namespace.hpp
@@ -0,0 +1,28 @@
+/*
+// gX Namespace
+*/
+
+#ifndef _GX_COMMON_NAMESPACE_
+#define _GX_COMMON_NAMESPACE_
+
+#ifndef _GX_NO_NAMESPACE_
+#   define _GX_NS              gx
+
+#   define _GX_BEGIN           namespace _GX_NS {
+#   define _GX_END             }
+#   define _GX_USE             using namespace _GX_NS;
+
+#   define _GX_INCLUDE_BEGIN   namespace _GX_INC_NAME {
+#   define _GX_INCLUDE_END     }
+#else
+#   define _GX_NS
+
+#   define _GX_BEGIN
+#   define _GX_END
+#   define _GX_USE
+
+#   define _GX_INCLUDE_BEGIN
+#   define _GX_INCLUDE_END
+#endif
+
+#endif
