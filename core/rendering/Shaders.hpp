136eadd4ae8c72db9f4d01b0c67b3ec24b66d5a5
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-10 22:29:15 +0100

    Create Shaders API

---
+++ core/rendering/Shaders.hpp
@@ -0,0 +1,50 @@
+/*
+// Graphics Shaders
+*/
+
+#ifndef _GX_RENDERING_SHADERS_HEADER_
+#define _GX_RENDERING_SHADERS_HEADER_
+
+/** Headers **/
+#include <Common.hpp>
+#include <maths/Math.hpp>
+
+_GX_BEGIN
+
+/**
+ * TODO:
+ *  OpenGL
+ *  Vulkan
+ **/
+class Shader
+{
+public:
+    virtual ~Shader() = default;
+
+    virtual void Bind() const = 0;
+    static void Reset();
+
+    virtual void SetUniformInt(const char* _sUniform, int _nData) = 0;
+    virtual void SetUniformIntArray(const char* _sUniform, int _nSize, const int* _pData) = 0;
+
+    virtual void SetUniformFloat(const char* _sUniform, float _fData) = 0;
+    virtual void SetUniformFloat2(const char* _sUniform, const Vec2& _vData) = 0;
+    virtual void SetUniformFloat3(const char* _sUniform, const Vec3& _vData) = 0;
+    virtual void SetUniformFloat4(const char* _sUniform, const Vec4& _vData) = 0;
+
+    virtual void SetUniformMat2(const char* _sUniform, const Mat2& _vData) = 0;
+    virtual void SetUniformMat3(const char* _sUniform, const Mat3& _vData) = 0;
+    virtual void SetUniformMat4(const char* _sUniform, const Mat4& _vData) = 0;
+
+    /*! @brief Create a new Shader from source
+    **/
+    static Reference<Shader> Create(const std::string& _sSource);
+
+    /*! @brief Create a new Shader from a file
+    **/
+    static Reference<Shader> CreateFromFile(const char* _sPath);
+};
+
+_GX_END
+
+#endif
