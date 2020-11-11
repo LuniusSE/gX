31c3a91d64f47fe5364b454ce2680e278b3fce6c
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-11 01:05:34 +0100

    Create GLArrays.hpp

---
+++ core/rendering/OpenGL/GLArrays.hpp
@@ -0,0 +1,22 @@
+/*
+// OpenGL Arrays
+*/
+
+#ifndef _GX_RENDERING_GLARRAYS_HEADER_
+#define _GX_RENDERING_GLARRAYS_HEADER_
+
+/** Headers **/
+#include <rendering/Arrays.hpp>
+
+_GX_REGION_BEGIN(OpenGL)
+
+class VertexArray : public gx::VertexArray
+{
+public:
+    VertexArray();
+    ~VertexArray();
+};
+
+_GX_REGION_END
+
+#endif
