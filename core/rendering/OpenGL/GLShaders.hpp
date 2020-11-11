5a8f9449002539de84700bb07f471cbd02394dcb
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-11 03:33:28 +0100

    Add Virtuals

--- core/rendering/OpenGL/GLShaders.hpp
+++ core/rendering/OpenGL/GLShaders.hpp
@@ -7,15 +7,39 @@
 
 /** Headers **/
 #include <rendering/Shaders.hpp>
+#include <string>
 
 _GX_REGION_BEGIN(OpenGL)
 
 class Shader : public gx::Shader
 {
+private:
+    gx::Program m_Program;
+    std::string m_Vertex, m_Fragment;
+
+    void CreateShader();
+
 public:
     Shader(const std::string& _sVertexSource, const std::string& _sFragmentSource);
     Shader(const char* _sPath);
     ~Shader();
+
+    virtual void Bind() const override;
+
+    gx::uInt GetLocation(const char* _sUniform);
+
+    virtual void SetUniformInt(const char* _sUniform, int _nData) override;
+    virtual void SetUniformIntArray(const char* _sUniform, gx::Count _uCount, const int* _pData) override;
+
+    virtual void SetUniformFloat(const char* _sUniform, float _fData) override;
+    virtual void SetUniformFloat2(const char* _sUniform, const gx::Vec2& _vData) override;
+    virtual void SetUniformFloat3(const char* _sUniform, const gx::Vec3& _vData) override;
+    virtual void SetUniformFloat4(const char* _sUniform, const gx::Vec4& _vData) override;
+
+    virtual void SetUniformMat2(const char* _sUniform, const gx::Mat2& _vData) override;
+    virtual void SetUniformMat3(const char* _sUniform, const gx::Mat3& _vData) override;
+    virtual void SetUniformMat4(const char* _sUniform, const gx::Mat4& _vData) override;
+    
 };
 
 _GX_REGION_END
