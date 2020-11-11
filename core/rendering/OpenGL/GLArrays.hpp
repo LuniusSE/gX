09a37527c16b9068aa6b6f4519cbbd4d8daee4af
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-11 01:47:26 +0100

    Add virtual functions

--- core/rendering/OpenGL/GLArrays.hpp
+++ core/rendering/OpenGL/GLArrays.hpp
@@ -6,15 +6,25 @@
 #define _GX_RENDERING_GLARRAYS_HEADER_
 
 /** Headers **/
+#include <Common.hpp>
 #include <rendering/Arrays.hpp>
+#include <rendering/Buffers.hpp>
 
 _GX_REGION_BEGIN(OpenGL)
 
 class VertexArray : public gx::VertexArray
 {
+private:
+    gx::Program m_Program;
+
 public:
     VertexArray();
     ~VertexArray();
+
+    virtual void Bind() const override;
+
+    virtual void AttachVertexBuffer(const gx::Scope<gx::VertexBuffer>& _pVertexBuffer) override;
+    virtual void AttachElementBuffer(const gx::Scope<gx::ElementBuffer>& _pElementBuffer) override;
 };
 
 _GX_REGION_END
