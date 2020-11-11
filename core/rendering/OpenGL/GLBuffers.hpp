5408f8fe121652dec750dee0d91812d474a4ebf6
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-11 02:16:38 +0100

    Add Virtuals

--- core/rendering/OpenGL/GLBuffers.hpp
+++ core/rendering/OpenGL/GLBuffers.hpp
@@ -13,16 +13,31 @@ _GX_REGION_BEGIN(OpenGL)
 
 class VertexBuffer : public gx::VertexBuffer
 {
+private:
+    gx::Program m_Program;
+    gx::Layout m_Layout;
+
 public:
     VertexBuffer(const std::vector<float>& _aVertices);
     ~VertexBuffer();
+
+    virtual void Bind() const override;
+
+    virtual void SetLayout(const gx::Layout& _aLayout) override;
+    virtual gx::Layout& GetLayout(void) override;
+
 };
 
 class ElementBuffer : public gx::ElementBuffer
 {
+private:
+    gx::Program m_Program;
+
 public:
     ElementBuffer(const std::vector<gx::Index>& _aElements);
     ~ElementBuffer();
+
+    virtual void Bind() const override;
 };
 
 _GX_REGION_END
