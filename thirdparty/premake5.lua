e47be44ea155819be7886880bcfeaf67aa48fdbf
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-11 21:02:58 +0100

    Include thirdparty projects

--- thirdparty/premake5.lua
+++ thirdparty/premake5.lua
@@ -1,26 +1,4 @@
-project "gX-ThirdParty"
-    kind "StaticLib"
-    language "C++"
-
-    objdir "../.build/obj/%{prj.name}"
-    targetdir "../.build/%{prj.name}"
-
-    files {
-        "**.h",
-        "**.c",
-        "**.cpp",
-        "**.hpp"
-    }
-
-    links {
-        "GL",
-        "dl"
-    }
-
-    filter "configurations:Debug"
-        defines { "GX_DEBUG" }
-        symbols "On"
-
-    filter "configurations:Release"
-        defines { "GX_RELEASE" }
-        optimize "On"
+include "glad"
+include "glfw"
+include "stb"
+include "glm"
