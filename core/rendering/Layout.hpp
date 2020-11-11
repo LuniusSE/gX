a9318aca703869b088bc288d6aba70b7d0bb3763
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-11 01:47:51 +0100

    Create Layout.hpp

---
+++ core/rendering/Layout.hpp
@@ -0,0 +1,37 @@
+/*
+// Graphics Shader Layout
+*/
+
+#ifndef _GX_RENDERING_LAYOUT_HEADER_
+#define _GX_RENDERING_LAYOUT_HEADER_
+
+/** Headers **/
+#include <Common.hpp>
+#include <vector>
+
+_GX_BEGIN
+
+/*! @brief Layout of VertexBuffers for Shaders.
+ * Implementation of Layouts vary between APIs (e.g. OpenGL, Vulkan)
+ */
+typedef enum class LayoutAttributes
+{
+    Int,
+
+    Float,
+    Float2,
+    Float3,
+    Float4,
+
+    Mat2,
+    Mat3,
+    Mat4
+}Attribute;
+
+/*! @brief Shader Layout type
+ **/
+using Layout = std::vector<Attribute>;
+
+_GX_END
+
+#endif
