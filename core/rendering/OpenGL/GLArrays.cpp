e61290df3ad053e5861fcd2fa1cd7fdeaed260e7
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-11 01:47:46 +0100

    Implement OpenGL VertexArray

---
+++ core/rendering/OpenGL/GLArrays.cpp
@@ -0,0 +1,42 @@
+/*
+// OpenGL Arrays
+*/
+#include "GLArrays.hpp"
+#include "GLLayout.hpp"
+#include <glad/glad.h>
+
+_GX_REGION_BEGIN(OpenGL)
+
+VertexArray::VertexArray()
+{
+    glCreateVertexArrays(1, &m_Program);
+}
+
+VertexArray::~VertexArray()
+{
+    glDeleteVertexArrays(1, &m_Program);
+}
+
+void VertexArray::VertexArray::Bind() const
+{
+    glBindVertexArray(m_Program);
+}
+
+void VertexArray::AttachVertexBuffer(const gx::Scope<gx::VertexBuffer>& _pVertexBuffer)
+{
+    /** Bind both VAO and VBO so OpenGL can attach them. **/
+    glBindVertexArray(m_Program);
+    _pVertexBuffer->Bind();
+
+    /** Set the VertexBuffer Layout **/
+    HandleVertexAttribPointers(_pVertexBuffer->GetLayout());
+}
+
+void VertexArray::AttachElementBuffer(const gx::Scope<gx::ElementBuffer>& _pElementBuffer)
+{
+    /** Bind both VAO and EBO so OpenGL can attach them. **/
+    glBindVertexArray(m_Program);
+    _pElementBuffer->Bind();
+}
+
+_GX_REGION_END
