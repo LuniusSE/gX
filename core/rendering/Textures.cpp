d74933f01f91f8c3352ad0d045d4e8ac91853404
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-11 01:04:42 +0100

    Create Textures.cpp

---
+++ core/rendering/Textures.cpp
@@ -0,0 +1,29 @@
+/*
+// Graphics Textures
+*/
+#include "Textures.hpp"
+
+/** Headers **/
+#include "OpenGL/GLTextures.hpp"
+
+_GX_BEGIN
+
+/**
+ * Texture2D::Create Implementation
+**/
+Reference<Texture2D> Texture2D::Create(/** Size, Data **/)
+{
+    /** TODO: Support GraphicsApi **/
+    return nullptr;
+}
+
+/**
+ * Texture2D::CreateFromFile Implementation
+**/
+Reference<Texture2D> Texture2D::CreateFromFile(const char* _sPath)
+{
+    /** TODO: Support GraphicsApi **/
+    return CreateRef<OpenGL::Texture2D>(_sPath);
+}
+
+_GX_END
