d345b56e16fffb4f047f71ba1437e32a3060afd1
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-13 02:22:38 +0100

    GLFW Window Implementation

---
+++ platform/GLFW/GLFWWindow.cpp
@@ -0,0 +1,53 @@
+/*
+// GLFW Window
+*/
+#include "GLFWWindow.hpp"
+#include "GLFWContext.hpp"
+
+_GX_REGION_BEGIN(GLFW)
+
+Window::Window(const std::string& _sTitle, gx::uInt _uWidth, gx::uInt _uHeight)
+    : m_Width(_uWidth), m_Height(_uHeight)
+{
+    m_pContext.reset(new Context(this));
+    
+    m_pWindow = glfwCreateWindow(m_Width, m_Height, _sTitle.c_str(), nullptr, nullptr);
+    if (m_pWindow == nullptr)
+    {
+        printf("[TEMP] GLFW\n%s\n", "Failed to create GLFWwindow");
+    }
+
+    m_pContext->CreateContext();
+}
+
+Window::~Window()
+{
+    glfwDestroyWindow(m_pWindow);
+}
+
+bool Window::IsOpen() const
+{
+    return (glfwWindowShouldClose(m_pWindow) == 0);
+}
+
+void Window::Update()
+{
+    glfwPollEvents();
+}
+
+gx::uInt Window::GetWidth() const
+{
+    return m_Width;
+}
+
+gx::uInt Window::GetHeight() const
+{
+    return m_Height;
+}
+
+gx::Platform::Context* Window::GetContext() const
+{
+    return m_pContext.get();
+}
+
+_GX_REGION_END
