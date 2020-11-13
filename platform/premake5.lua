d35e2785dced91a9832c9064e167e5f7a84c9869
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-13 02:22:00 +0100

    Depend on GLFW and include Core

--- platform/premake5.lua
+++ platform/premake5.lua
@@ -3,35 +3,40 @@ project "gX-Platform"
     language "C++"
 
     dependson {
+        "gXtp-glfw",
         "gX-Core"
     }
 
     objdir "%{wks.location}/.build/obj/%{prj.name}"
     targetdir "%{wks.location}/.build/%{prj.name}"
 
-    includedirs { "%{wks.location}/core" };
+    includedirs {
+        "%{wks.location}/thirdparty",
+        "%{wks.location}/platform", 
+        "%{wks.location}/core", 
+        "%{wks.location}/core/common" 
+    };
 
     files {
         "**.cpp",
         "**.hpp"
     }
+    
+    links {
+        "gXtp-glfw"
+    }
 
     filter "system:linux"
         defines { "GX_SYS_LINUX" }
-        links {
-            "X11"
-        }
+        links { "X11" }
 
     filter "system:windows"
         defines { "GX_SYS_WINDOWS" }
-        links {
-        }
+        links { }
 
     filter "system:macosx"
         defines { "GX_SYS_MACOSX" }
-        links {
-            "X11"
-        }
+        links { "X11" }
 
     filter "configurations:Debug"
         defines { "GX_DEBUG" }
