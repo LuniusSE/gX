f85de5579a478daac0e2157450da54d62bc535cf
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-13 20:56:06 +0100

    Implement CreateTextureFromData

--- core/rendering/OpenGL/GLTextures.cpp
+++ core/rendering/OpenGL/GLTextures.cpp
@@ -9,6 +9,12 @@
 
 _GX_REGION_BEGIN(OpenGL)
 
+Texture2D::Texture2D(gx::Vec2 _vSize, const gx::uChar* _pData)
+    : m_Width(_vSize.x), m_Height(_vSize.y)
+{
+    CreateTextureFromData(_pData);
+}
+
 Texture2D::Texture2D(const char* _sPath)
     : m_Path(_sPath)
 {
@@ -18,6 +24,23 @@ Texture2D::Texture2D(const char* _sPath)
 Texture2D::~Texture2D()
 {
     glDeleteTextures(1, &m_Program);
+}
+
+void Texture2D::CreateTextureFromData(const gx::uChar* _pData)
+{
+    /** Create Texture **/
+    glCreateTextures(GL_TEXTURE_2D, 1, &m_Program);
+    glTextureStorage2D(m_Program, 1, GL_RGBA8, m_Width, m_Height);
+
+    /** Texture Options **/
+    glTextureParameteri(m_Program, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTextureParameteri(m_Program, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTextureParameteri(m_Program, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTextureParameteri(m_Program, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    /** Set Texture data **/
+    glTextureSubImage2D(m_Program, 0, 0, 0, m_Width, m_Height, GL_RGBA, GL_UNSIGNED_BYTE, _pData);
 }
 
 void Texture2D::CreateTextureFromFile()
