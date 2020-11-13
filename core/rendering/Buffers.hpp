91a31613112221bae58c71adcd75397565d374ca
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-13 20:53:40 +0100

    Change how VertexBuffers are created

--- core/rendering/Buffers.hpp
+++ core/rendering/Buffers.hpp
@@ -28,8 +28,19 @@ public:
     virtual Layout& GetLayout(void) = 0;
 
     /*! @brief Create a new VertexBuffer Object
+     *
+     * @param[in] _uSize sizeof(type) * count
+     * 
     **/
-    static Reference<VertexBuffer> Create(const std::vector<float>& _aVertices);
+    static Reference<VertexBuffer> Create(Size _uSize);
+
+    /*! @brief Create a new VertexBuffer Object
+     *
+     * @param[in] _pVertices raw pointer to data
+     * @param[in] _uSize sizeof(type) * count
+     * 
+    **/
+    static Reference<VertexBuffer> Create(const void* _pVertices, Size _uSize);
 };
 
 /**
