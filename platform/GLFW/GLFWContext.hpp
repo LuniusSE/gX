44799b74ffaed700571447dc6bddb40929635164
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-13 02:22:48 +0100

    Create GLFW Context

---
+++ platform/GLFW/GLFWContext.hpp
@@ -0,0 +1,32 @@
+/*
+// GLFW Context
+*/
+
+#ifndef _GX_PLATFORM_GLFW_CONTEXT_HEADER_
+#define _GX_PLATFORM_GLFW_CONTEXT_HEADER_
+
+/** Headers **/
+#include <Common.hpp>
+#include <platform/Context.hpp>
+#include "GLFWWindow.hpp"
+
+_GX_REGION_BEGIN(GLFW)
+
+class Context : public gx::Platform::Context
+{
+private:
+    Window* m_Window;
+    // GraphicsContext*
+
+public:
+    Context(gx::Platform::Window* _pGLFWWindow);
+    ~Context();
+
+    virtual void CreateContext() override;
+    virtual void SwapBuffers() override;
+
+};
+
+_GX_REGION_END
+
+#endif
