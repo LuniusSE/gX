73a7bb425f6f349663d2b0260890592b497fd6a6
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-13 20:55:48 +0100

    Implement new creation method

--- core/rendering/Textures.cpp
+++ core/rendering/Textures.cpp
@@ -11,16 +11,16 @@ _GX_BEGIN
 /**
  * Texture2D::Create Implementation
 **/
-Reference<Texture2D> Texture2D::Create(/** Size, Data **/)
+Reference<Texture2D> Texture2D::Create(Vec2 _vSize, const uChar* _pData)
 {
     /** TODO: Support GraphicsApi **/
-    return nullptr;
+    return CreateRef<OpenGL::Texture2D>(_vSize, _pData);
 }
 
 /**
  * Texture2D::CreateFromFile Implementation
 **/
-Reference<Texture2D> Texture2D::CreateFromFile(const char* _sPath)
+Reference<Texture2D> Texture2D::Create(const char* _sPath)
 {
     /** TODO: Support GraphicsApi **/
     return CreateRef<OpenGL::Texture2D>(_sPath);
