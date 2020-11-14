9e072cc793a8c8f3d8a56781104026199d782fc7
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-15 00:47:10 +0100

    Include Events

--- core/common/Common.hpp
+++ core/common/Common.hpp
@@ -16,4 +16,11 @@
 /** Maths **/
 #include <maths/Types.hpp>
 
+/** Events **/
+#include <io/Events.hpp>
+#include <io/EventMessanger.hpp>
+#include <io/Events/KeyEvents.hpp>
+#include <io/Events/MouseEvents.hpp>
+#include <io/Events/WindowEvents.hpp>
+
 #endif
