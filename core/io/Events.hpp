41fe07bc84e88116c98c2cd4a075ae6546526d58
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-15 00:46:19 +0100

    Create Events.hpp

---
+++ core/io/Events.hpp
@@ -0,0 +1,73 @@
+/*
+// Events
+*/
+
+#ifndef _GX_IO_EVENTS_
+#define _GX_IO_EVENTS_
+
+/** Includes **/
+#include <Common.hpp>
+
+_GX_BEGIN
+
+/**
+ * Event Classifications
+ **/
+enum Events
+{
+    Key,
+    Char,
+    Mouse,
+    Motion,
+    Scroll,
+
+    Window,
+};
+
+/**
+ * Types of Events
+ **/
+enum class EventTypes
+{
+    KeyPressed,
+    KeyReleased,
+    KeyRepeat,
+
+    MouseButtonPressed,
+    MouseButtonReleased,
+
+    MouseMotion,
+    MouseScrolled,
+
+    WindowResized,
+    WindowMoved,
+    WindowClosed
+};
+
+/**
+ * 
+ **/
+class Event
+{
+friend class EventMessanger;
+private:
+    bool m_EventHandled;
+
+public:
+    inline Event() : m_EventHandled(false) {}
+    virtual ~Event() = default;
+
+    virtual EventTypes GetEventType() const = 0;
+    virtual uInt GetEventClass() const = 0;
+
+    inline bool IsEventClass(const uInt& _eClass) const
+    { return (GetEventClass() & _eClass); }
+
+    inline bool IsHandled() const
+    { return m_EventHandled; }
+
+};
+
+_GX_END
+
+#endif
