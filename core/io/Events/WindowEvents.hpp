79759d6d94dc61b0386fb91a6dc89d95a939682e
LuniusSE <73563735+LuniusSE@users.noreply.github.com>
2020-11-15 00:46:31 +0100

    Create WindowEvents.hpp

---
+++ core/io/Events/WindowEvents.hpp
@@ -0,0 +1,93 @@
+/*
+// Events
+*/
+
+#ifndef _GX_IO_WINDOW_EVENTS_
+#define _GX_IO_WINDOW_EVENTS_
+
+/** Includes **/
+#include <Common.hpp>
+#include <io/Events.hpp>
+
+_GX_BEGIN
+
+/**
+ * 
+ **/
+class WindowResizedEvent : public Event
+{
+protected:
+    uInt m_Width, m_Height;
+
+public:
+    inline WindowResizedEvent(const uInt& _Width, const uInt& _Height)
+        : m_Width(_Width), m_Height(_Height) {}
+
+    inline uInt GetWidth() const 
+    { return m_Width; }
+
+    inline uInt GetHeight() const 
+    { return m_Height; }
+
+    inline virtual uInt GetEventClass() const override
+    { return Events::Window; }
+
+    inline static EventTypes GetStaticType()
+    { return EventTypes::WindowResized; }
+
+    inline virtual EventTypes GetEventType() const override
+    { return GetStaticType(); }
+
+};
+
+/**
+ * 
+ **/
+class WindowMovedEvent : public Event
+{
+protected:
+    uInt m_X, m_Y;
+
+public:
+    inline WindowMovedEvent(const uInt& _X, const uInt& _Y)
+        : m_X(_X), m_Y(_Y) {}
+
+    inline uInt GetX() const 
+    { return m_X; }
+
+    inline uInt GetY() const 
+    { return m_Y; }
+
+    inline virtual uInt GetEventClass() const override
+    { return Events::Window; }
+
+    inline static EventTypes GetStaticType()
+    { return EventTypes::WindowMoved; }
+
+    inline virtual EventTypes GetEventType() const override
+    { return GetStaticType(); }
+
+};
+
+/**
+ * 
+ **/
+class WindowClosed: public Event
+{
+public:
+    inline WindowClosed() {}
+
+    inline virtual uInt GetEventClass() const override
+    { return Events::Window; }
+
+    inline static EventTypes GetStaticType()
+    { return EventTypes::WindowClosed; }
+
+    inline virtual EventTypes GetEventType() const override
+    { return GetStaticType(); }
+
+};
+
+_GX_END
+
+#endif
